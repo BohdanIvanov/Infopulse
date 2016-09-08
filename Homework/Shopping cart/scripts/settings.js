1cd8620e099d196d52dc3688df8780a97a2187c4
BohdanIvanov <bondiboss7@gmail.com>
2016-09-08 22:59:49 +0300

    Remove
    Removed unnecessary files

--- Homework/Shopping cart/scripts/settings.js
+++
@@ -1,8 +0,0 @@
-var settings = {};
-settings.data = null;
-settings.init = function(oSettings){
-    settings.data = oSettings;
-}
-settings.get = function(sSettingName){
-    return settings.data[sSettingName];
-}
