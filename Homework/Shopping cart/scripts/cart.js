3d6b79c4126a0a526485072fbba3853bcc81f713
BohdanIvanov <bondiboss7@gmail.com>
2016-09-12 19:04:39 +0300

    Update cart.js
    Added some improvements

--- Homework/Shopping cart/scripts/cart.js
+++ Homework/Shopping cart/scripts/cart.js
@@ -4,11 +4,11 @@ function cart(sSelector, sMinicartSelector){
 	// 1. Data ====================
 	c.init(sSelector);
 	
-	c.minicart = $(sMinicartSelector);
-    c.goods = c.minicart.find('.b-goods_cart');             // table goods in cart
-    c.list = c.minicart.find('.b-minicart__list');          // list of goods in cart
-    c.button = c.minicart.find('.b-minicart__button');
-	c.cartData = { /*  iGoodId : iQty */ };
+	c.minicart 	= $(sMinicartSelector);
+    c.goods 	= c.minicart.find('.b-goods_cart');             // table goods in cart
+    c.list 		= c.minicart.find('.b-minicart__list');          // list of goods in cart
+    c.button 	= c.minicart.find('.b-minicart__button');
+	c.cartData 	= { /*  iGoodId : iQty */ };
 	
 	// 2. Logic ===================
 	c.add = function (event){
@@ -25,56 +25,85 @@ function cart(sSelector, sMinicartSelector){
         c.list.stop().slideDown();
 		}
     
-   c.increase = function(){
-        var jqIncrease = $(this)
-        ,   jqForm = jqIncrease.closest('.b-order-form')
-        ,   jqCurrentValue = jqForm.find('.b-order-form__quantity')
-        ,   newValue = parseInt(jqCurrentValue.val());
-        if(newValue < 10){
-            jqCurrentValue.val(newValue + 1);
-        }
+/* 
+// first option
+	c.changeQty = function (oButton, iShift){
+		// $(this) - this ссылается на текущий класс cart (см. this1.png)
+	    var jqIncrease = $(oButton)
+			,   jqForm = jqIncrease.closest('.b-order-form')
+			,   jqCurrentValue = jqForm.find('.b-order-form__quantity')
+			,   iNewValue = parseInt(jqCurrentValue.val()) + iShift;	
 
+        if(iNewValue >= 1 && iNewValue <= 10){
+			jqCurrentValue.val(iNewValue);
+			}
+		}
+   c.increase = function(){
+		c.changeQty(this, +1);
     }
         
     c.decrease = function(){
-        var jqIncrease = $(this)
-        ,   jqForm = jqIncrease.closest('.b-order-form')
-        ,   jqCurrentValue = jqForm.find('.b-order-form__quantity')
-        ,   newValue = parseInt(jqCurrentValue.val());
-        if(newValue > 1){
-            jqCurrentValue.val(newValue - 1);
-        }   
-    }
+		c.changeQty(this, -1);
+    } */
     
-    c.put = function(jqGood){
-        	var		
+	// second option
+    c.changeQty = function (){
+		// this refers on buttons +/- 
+	    var jqButton = $(this)
+			,   jqForm = jqButton.closest('.b-order-form')
+			,   jqCurrentValue = jqForm.find('.b-order-form__quantity')
+			,   iNewValue = parseInt(jqCurrentValue.val()) + (
+					jqButton.hasClass('b-order-form__plus')
+						? +1 
+						: -1
+					);	
+
+        if(iNewValue >= 1 && iNewValue <= 10){
+			jqCurrentValue.val(iNewValue);
+			}
+		}
+    c.put = function(jqGood, bDelete){
+		bDelete = bDelete || false;
+		
+        var		
 			 iGoodId    = jqGood.attr('class').match(/b-good_id_([0-9]{1,8})/)[1]
 			,iQty		= jqGood.find('.b-order-form__quantity').val()
-		  , sGoodIdClassName = 'b-good_id_' + iGoodId
-		c.cartData[iGoodId] = iQty;
+			, sGoodIdClassName = 'b-good_id_' + iGoodId
+			
+		if (bDelete){		// if delete
+			jqGood
+				.removeClass('b-good_in-cart')
+				.find('.b-order-form__quantity').val(1)
+				;
+			
+			delete  c.cartData[iGoodId];
+			}
+		else {		// if NOT delete, if PUT
+			c.cartData[iGoodId] = iQty;
+			jqGood.addClass('b-good_in-cart');
+			
+			var jqCartGood = c.goods.find('.' + sGoodIdClassName)     //    find current good in the cart      
+			
+			if(!jqCartGood.length){         // no goods in cart
+				//склонировать шаблонный ряд
+				jqCartGood = c.goods.find('.b-good:first-child').clone(true/*also clone events*/).appendTo(c.goods); //copy of object in the memory
+				//fill it with data from the product catalog
+				//read the data from catalog
+		   
+				//to write                              //to read
+				jqCartGood.find('.b-good__name').html( jqGood.find('.b-good__name').html() ); // name
+				jqCartGood.find('.b-good__price').html( jqGood.find('.b-good__price').html()); // price
+				jqCartGood.find('.b-good__image').attr('src', jqGood.find('.b-good__image').attr('src')); // photo
+				jqCartGood.addClass(sGoodIdClassName);
+			}
+			
+			// the product is already in cart
+			jqCartGood.find('.b-order-form__quantity').val( jqGood.find('.b-order-form__quantity').val());//quantity
+						
+			}
         
-        Cookies.set('goods', c.cartData, { expires: 14, path: '/' }); //    name of cookie choosing for yourself
-        jqGood.addClass('b-good_in-cart');
-        
-        var jqCartGood = c.goods.find('.' + sGoodIdClassName)     //    find current good in the cart      
-        
-        if(!jqCartGood.length){         // no goods in cart
-            //склонировать шаблонный ряд
-            jqCartGood = c.goods.find('.b-good:first-child').clone(true).appendTo(c.goods); //copy of object in the memory
-            //fill it with data from the product catalog
-            //read the data from catalog
-       
-            //to write                              //to read
-            jqCartGood.find('.b-good__name').html( jqGood.find('.b-good__name').html() ); //название
-            jqCartGood.find('.b-good__price').html( jqGood.find('.b-good__price').html()); //цена
-            jqCartGood.find('.b-good__image').attr('src', jqGood.find('.b-good__image').attr('src')); //фото
-            jqCartGood.addClass(sGoodIdClassName);
-            
-        }
-        
-        // the product is already in cart
-        jqCartGood.find('.b-order-form__quantity').val( jqGood.find('.b-order-form__quantity').val());//количество
-        
+        Cookies.set('goods', c.cartData, { expires: 14, path: '/' }); //    name of cookie is choosen by yourself
+   
     }
     c.toggleList = function(){
         c.list.slideToggle();
@@ -82,18 +111,14 @@ function cart(sSelector, sMinicartSelector){
     
       
      c.remove = function(){
-        var currentCart = $(this)
-        , newCart = currentCart.closest('.b-good');
-        newCart.remove();
-        var iGoodId  = newCart.attr('class').match(/b-good_id_([0-9]{1,8})/)[1]
-        , sGoodIdClassName = 'b-good_id_' + iGoodId
-        , iQty		= newCart.find('.b-order-form__quantity').val()
-        , removeGood = c.find('.' + sGoodIdClassName);
-        removeGood.removeClass('b-good_in-cart');
-        removeGood.find('.b-order-form__quantity').val(1);
-        delete  c.cartData[iGoodId];
-        Cookies.set('goods', c.cartData, { expires: 14, path: '/' });
-         
+        // var jqDeleteButton = $(this)
+        var jqCartGood = $(this).closest('.b-good');
+        
+        var 
+			jqGood = c.find('.' + jqCartGood.attr('class').match(/(b-good_id_[0-9]{1,8})/)[1])
+			jqCartGood.remove();	
+			
+		c.put(jqGood, true);
          
         }
     
@@ -117,10 +142,16 @@ function cart(sSelector, sMinicartSelector){
 	// 3. Events ==================
 	c.find('.b-order-form').on('submit', c.add);
     c.button.on('click', c.toggleList);
-    c.find('.b-order-form__plus').on('click', c.increase);
-    c.find('.b-order-form__minus').on('click', c.decrease);
+	// First option
+    // c.find('.b-order-form__plus') .add(c.minicart.find('.b-order-form__plus'))	.on('click', c.increase);
+    // c.find('.b-order-form__minus').add(c.minicart.find('.b-order-form__minus'))	.on('click', c.decrease);
+	
+    // Second option
+	// to cart object (c.elem) Adding minicart object, searching buttons "+" и "-"....
+	c.elem.add(c.minicart).find('.b-order-form__plus, .b-order-form__minus').on('click', c.changeQty);
+
     c.minicart.find('.b-good__delete').on('click', c.remove);
     $(document).ready(c.load);                              //When document is ready - load a cart
-}                                       
+}                                      
 
 cart.prototype = new Component();
